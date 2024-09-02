57f7046df1677d5a8396382985c5162c117eecd5
laon makers <laon.makers@yahoo.com>
2024-09-02 01:34:17 -0400

    version 1.4: it gets any html tag, e.g. <br>, <i></i>, and etc..., within a text in any data js file worked. version: 1.5: project names on the dashboard have been changed. The 3rd day status has been updated. The 4th day status has been updated. The 5th status update. The 6th day update. The induced current. The 7th day update. Review of S/W design. How both motor and H-bridge work. The 8th day update. Demo of the motor contorol simulator and IPCs. The 9th day update: how to design the smart door lock sub-system after reviewing feature requirements. The 10th day update: Design approaches and how to simplify requirements by grouping features into associated functions. The 11th day update: creating H/W high-level block diagram and reviewing bit-field and its use cases. The 12th day update: reviewed the last one and continued working on H/W high level block diagram. The 13th day update: reviewed the last one and continued working on H/W high level block diagram. Discussed the concept of period, frequency, modulation, and time/frequency domain. The 14th day update: refreshed memory, completed high level H/W block diagram, and listed the required communication peripherals. Reviewed a datasheet to check if it supported the required peripherals. The 15the day update: reviewed the datasheet of Arduino Mega128 in details to learn about system clock, MICS, RISC, etc... The 16th day update: continued datasheet review; MCU vs CPU, Peripherals, External/Interal Interrupt Sources, etc.. The 17th day update: KiCAD 6.0 configuration. Private symbol library added. Pull up/down resistor explained.

---
+++ data/sw77686C65653438_data.js
@@ -0,0 +1,21 @@
+let refRt = ["1-38-35"];
+let dateTimeLog = [
+    [1,  "2025.02.16", "14:00", "16:20", "2:20",  -1,     0,      2,   2, "Drawing schematic with KiCad 6.0<br> <span style='color:grey;'>(configuring &amp; drawing)</span>"],
+    [1,  "2025.02.09", "14:00", "16:10", "2:10",  -1,    21,      4,   2, "Reviewed Arduino Mega128 datasheet in details<br> <span style='color:grey;'>(CPU vs MCU, Peripherals, Interrupt sources, ...)</span>"],
+    [1,  "2025.02.02", "14:00", "16:00", "2:00",  -1,     0,      0,   2, "Reviewed Arduino Mega128 datasheet in details<br> <span style='color:grey;'>(Single Clock Cycle, MIPS, RISC, ...)</span>"],
+    [1,  "2025.01.26", "14:00", "16:10", "2:00",  -1,     0,      2,   2, "Refreshed memory &amp; completed high level H/W block diagram<br> <span style='color:grey;'>(1 x UART, 2 x I2C, 2 x SPI)</span>, and reviewed a data sheet <span style='color:grey;'>(BeagleBone)</span>."],
+    [1,  "2025.01.12", "14:00", "16:00", "2:00",  -1,    21,      4,   2, "Refreshed memory &amp; continued high level H/W block diagram: <br><span style='color:grey;'>(Modulation: PWM, AM, FM. Period &amp; Frequency Domain.)</span>"],
+    [1,  "2025.01.05", "14:00", "16:15", "2:15",  -1,     0,      0,   2, "Refreshed memory &amp; continued high level H/W block diagram: <br><span style='color:grey;'>(PWM,duty cycle, active low LED control)</span>"],
+    [1,  "2024.12.15", "17:40", "18:50", "1:10",  -1,     0,      2,   0, "<span style='color:grey;'>Free( bit-field and its use cases ).</span>"],
+    [1,  "2024.12.15", "14:15", "16:15", "2:00",  -1,     0,      2,   2, "Creating high level H/W block diagram based on the Requirement Document."],
+    [1,  "2024.12.08", "14:00", "16:10", "2:10",  -1,     0,      4,   2, "Design approaches and how to simplify requirements by grouping features into associated functions."],
+    [1,  "2024.12.01", "14:10", "16:20", "2:10",  -1,     0,      6,   2, "How to design smart door lock sub-system after reviewing feature requirements."],
+    [1,  "2024.11.24", "14:00", "16:00", "2:00",  -1,     0,      8,   2, "Simulation &amp; emulation, backed by the demo of the motor control simulation.<br>Socket, pipe, and shared memory as Inter Process Communication(IPC)."],
+    [1,  "2024.11.03", "14:00", "16:00", "2:00",  -1,    42,     10,   2, "Motor control S/W design result review.<br>How motor and H-bridge driver work. &ensp; <i style='color:grey; font-size:small;'>--10월11일 6회분 납부완료--</i>"],
+    [1,  "2024.09.29", "14:00", "16:35", "2:15",  -1,     0,      0,   2, "Capacitor: how the induced current flows.<br>What is the relay in a circuit?"],
+    [1,  "2024.09.22", "14:00", "16:00", "2:00",  -1,     0,      2,   2, "Hands-on experimentation on the capacitor reaction differences between during the voltage <br>accross the capacitor being changed and not changed."],
+    [1,  "2024.09.15", "14:00", "16:10", "2:10",  -1,     0,      4,   2, "Built and downloaded a RFID reference project to Arduino UNO. Brief introduction to RFID. <br> The characteristics of capacitors <i style='color:grey;'>(with the story of  '견우&amp;직녀')</i>"],
+    [1,  "2024.09.08", "14:05", "16:40", "2:35",  -1,     0,      6,   2, "The needs of system state sync, GPIO disable/enable, Filp-flop reset"],
+    [1,  "2024.09.01", "13:45", "16:15", "2:30",  -1,     0,      8,   2, "Filp-flop/FFT, AM/FM, Amp, The birth of CPU"],
+    [1,  "2024.08.25", "14:00", "16:00", "2:00",  -1,    42,     10,   2, "First Day.<br><i style='color:grey;'>궁녀와 장인, V,I,R, ArduinoRFID</i>"]
+];
