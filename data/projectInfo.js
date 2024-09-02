6c849853381079befb48e9a756f31c973e90947e
laon makers <laon.makers@yahoo.com>
2024-09-02 02:34:43 -0400

    version: 1.5: project names on the dashboard have been changed.

--- data/projectInfo.js
+++ data/projectInfo.js
@@ -1,11 +1,11 @@
 let projects = [
-    {"name":"Dashboard",           "color":"red",    "progress":100, "kick-off":"Dec. 18, 2021", "Design":1,   "Development":10, "Test & Debug":4, "Change Request":3, "Customer Service":0},
-    {"name":"Ultrasonic Keyboard", "color":"orange", "progress":100, "kick-off":"Mar. 14, 2021", "Design":3,   "Development":15, "Test & Debug":6, "Change Request":5, "Customer Service":5},
-    {"name":"Flower",              "color":"cyan",   "progress":100, "kick-off":"N/A",           "Design":2,   "Development":6,  "Test & Debug":2, "Change Request":0, "Customer Service":0},
-    {"name":"Interactive LED",     "color":"green",  "progress":100, "kick-off":"Sep. 20, 2021", "Design":17,  "Development":70, "Test & Debug":16,"Change Request":10, "Customer Service":10},
-    {"name":"Reflective Wheel",    "color":"brown",  "progress":25,  "kick-off":"Dec. 22, 2021", "Design":19,   "Development":0,  "Test & Debug":0, "Change Request":0, "Customer Service":0},
-    {"name":"Spinning blocks",     "color":"navy",   "progress":1,   "kick-off":"Dec. 18, 2021", "Design":0.1, "Development":0,  "Test & Debug":0, "Change Request":0, "Customer Service":0},
-    {"name":"Painting Light",      "color":"purple", "progress":5,   "kick-off":"Jan. 29, 2022", "Design":2,   "Development":0,  "Test & Debug":0, "Change Request":0, "Customer Service":0},
-    {"name":"NFT Shop",            "color":"blue",   "progress":1,   "kick-off":"Dec. 18, 2021", "Design":0.5, "Development":0,  "Test & Debug":0, "Change Request":0, "Customer Service":0}
+    {"name":"Dashboard",           "color":"red",    "progress":0.1, "kick-off":"N/A", "Design":0.1,   "Development":0, "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Arduino RFID", "color":"orange", "progress":5, "kick-off":"Aug. 25, 2024", "Design":5,   "Development":0, "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Motion Detection",              "color":"cyan",   "progress":0.1, "kick-off":"N/A",           "Design":0.1,   "Development":0,  "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Mini Database",     "color":"green",  "progress":0.1, "kick-off":"N/A", "Design":0.1,  "Development":0, "Test & Debug":0,"Change Request":0, "Customer Service":0},
+    {"name":"Smart Power Mgr.",    "color":"brown",  "progress":0,  "kick-off":"N/A", "Design":0,   "Development":0,  "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Door Keypad",     "color":"navy",   "progress":0,   "kick-off":"N/A", "Design":0.1, "Development":0,  "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"WiFi Smart Door",      "color":"purple", "progress":0.1,   "kick-off":"N/A", "Design":0.1,   "Development":0,  "Test & Debug":0, "Change Request":0, "Customer Service":0},
+    {"name":"Esp32 Security Cam",            "color":"blue",   "progress":0,   "kick-off":"N/A", "Design":0, "Development":0,  "Test & Debug":0, "Change Request":0, "Customer Service":0}
 ];
 
