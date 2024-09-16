12cddebcdd88ef182753e672f0d6f3604b97f842
gtcGameNb <laon.makers@yahoo.com>
2024-09-15 20:16:22 -0400

    The 4th day status has been updated.

--- data/sw77686C65653438_data.js
+++ data/sw77686C65653438_data.js
@@ -1,5 +1,6 @@
 let refRt = ["1-38-35"];
 let dateTimeLog = [    
+    [1,  "2024.09.15", "14:00", "16:10", "2:10",  -1,     0,      4,   2, "Built and downloaded a RFID reference project to Arduino UNO. Brief introduction to RFID. <br> The characteristics of capacitors <i style='color:grey;'>(with the story of  '견우&amp;직녀')</i>"],
     [1,  "2024.09.08", "14:05", "16:40", "2:35",  -1,     0,      6,   2, "The needs of system state sync, GPIO disable/enable, Filp-flop reset"],
     [1,  "2024.09.01", "13:45", "16:15", "2:30",  -1,     0,      8,   2, "Filp-flop/FFT, AM/FM, Amp, The birth of CPU"],
     [1,  "2024.08.25", "14:00", "16:00", "2:00",  -1,    42,     10,   2, "First Day.<br><i style='color:grey;'>궁녀와 장인, V,I,R, ArduinoRFID</i>"]
