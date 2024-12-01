59dad36167dec013ec5c97cabe2e4b98a0055ece
gtcGameNb <laon.makers@yahoo.com>
2024-12-01 16:38:10 -0500

    The 9th day update: how to design the smart door lock sub-system after reviewing feature requirements.

--- data/sw77686C65653438_data.js
+++ data/sw77686C65653438_data.js
@@ -1,5 +1,6 @@
 let refRt = ["1-38-35"];
 let dateTimeLog = [    
+    [1,  "2024.12.01", "14:10", "16:20", "2:10",  -1,     0,      6,   2, "How to design smart door lock sub-system after reviewing feature requirements."],
     [1,  "2024.11.24", "14:00", "16:00", "2:00",  -1,     0,      8,   2, "Simulation &amp; emulation, backed by the demo of the motor control simulation.<br>Socket, pipe, and shared memory as Inter Process Communication(IPC)."],
     [1,  "2024.11.03", "14:00", "16:00", "2:00",  -1,    42,     10,   2, "Motor control S/W design result review.<br>How motor and H-bridge driver work. &ensp; <i style='color:grey; font-size:small;'>--10월11일 6회분 납부완료--</i>"],
     [1,  "2024.09.29", "14:00", "16:35", "2:15",  -1,     0,      0,   2, "Capacitor: how the induced current flows.<br>What is the relay in a circuit?"],
