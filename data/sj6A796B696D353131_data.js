2bb977334ad647145c9cce9902e2d7654ede1810
gtcGameNb <laon.makers@yahoo.com>
2025-11-23 17:51:32 -0500

    The 1st day update: C basics and two high level project designs (GgoMee and GgoMee Sniffer project). The 2nd day update: C language structure in depth. Variable and function names. Pre-processors and data types. The 3rd day update: details of data types. VIR, Vcc, GND via the story of water powred fan invention. Blink prj and modification. The 4th day update: bit operations, operation orders, variable scopes, value assginments (overflow and loss of decimal/fractional part; loss of value flavor). The 5th day update: blink example and its application. How to analize the blink in terms of the voltage and current. The 'if' statement and its usage. The 6th day update: repeat the process of turning the green light on for 2 seconds, then the red light on for 2 seconds. The 7th day update: add the yellow light to the traffic control project. Install bit and learn how to use it. The 8th day update: add the traffic light control button and control the light's on/off cycle with the button. The 9th day update: address the issue where the button press doesn't stop the traffic light on/off cycle. The 10th day: pull up/down resistor. Code optimization. Git server. The 11th day: move a few function into new files; one source and one header. Use a VS Code as new IDE.

---
+++ data/sj6A796B696D353131_data.js
@@ -0,0 +1,14 @@
+let refRt = ["1-38-30"];
+let dateTimeLog = [
+    [11, "2026. 3.08", "14:00", "18:30", "4:30",  -1,    20,     7,       3, "<i>Move a few function into newly created files; one source and one header file.<br>Use the VS code as an IDE.</i>"],
+    [10, "2026. 2.22", "14:00", "19:00", "5:00",  -1,     0,     3.333,   3, "<i>Pull up/down resistor.Code optimization with a function. Made a git server.</i>"],
+    [9,  "2026. 2.16", "14:00", "18:00", "4:00",  -1,    20,     6.333,   3, "<i>Address the issue where the button press doesn't stop the traffic light on/off cycle.<br>Review the data type definition/declaration and scopes. Configure the git difftool.</i>"],
+    [8,  "2026. 2.08", "14:00", "18:20", "4:20",  -1,     0,     2.667,   3, "<i>Add the traffic light control button and control the light on/off cycle with the button.</i>"],
+    [7,  "2026. 2.01", "14:00", "17:40", "3:40",  -1,    20,     5.667,   3, "<i>Added the yellow light to the traffic lights. Installed git and committed changes.</i>"],
+    [6,  "2025.12.28", "14:15", "18:00", "3:45",  -1,     0,     2,       3, "<i>Controlling the traffic lights.</i><br><i style='color:grey;'>Repeat the process of turning the green light on for 2 seconds,<br>then the red light on for 2 seconds.</i>"],
+    [5,  "2025.12.21", "14:00", "18:00", "4:00",  -1,    20,     5,       3, "<i>Blink example and its application.</i><br><i style='color:grey;'>How it is blinked with voltage (the height of waterfall) and<br> the current (the flow of water). The 'if' statement and its application.</i>"],
+    [4,  "2025.12.14", "15:00", "18:30", "3:30",  -1,     0,     1.334,   3, "<i style='color:grey;'>Bit operations, operation orders, variable scopes, <br>value assignments: overflow &amp; loss of flavor (loss of decimal/fractional part).</i>"],
+    [3,  "2025.12.07", "14:00", "17:40", "3:40",  -1,    20,     4.334,   3, "<i style='color:grey;'>Data type details(variable name == '문패').<br> VIR, Vcc, and GND via '궁녀와장인'. Blink example prj.</i>"],
+    [2,  "2025.11.30", "14:00", "17:30", "3:30",  -1,     0,     0.667,   3, "<i style='color:grey;'>Variable and Function (names and structure),<br> pre-processor, data types</i>"],
+    [1,  "2025.11.23", "14:00", "17:30", "3:30",  -1,    20,     3.667,   3, "<i style='color:grey;'>C Basics. High level project design(꼬미 and <br>꼬미 sniffer project)</i>"]
+];
