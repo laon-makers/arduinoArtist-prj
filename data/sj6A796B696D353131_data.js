aacc6b264b42b7430d81f88ffe9bdc79731a5d60
gtcGameNb <laon.makers@yahoo.com>
2025-11-23 17:51:32 -0500

    The 1st day update: C basics and two high level project designs (GgoMee and GgoMee Sniffer project).

---
+++ data/sj6A796B696D353131_data.js
@@ -0,0 +1,4 @@
+let refRt = ["1-38-30"];
+let dateTimeLog = [
+    [1,  "2025.11.23", "14:00", "17:30", "3:30",  -1,    20,     3.667,   3, "First Day.<br><i style='color:grey;'>C Basics. High level project design(꼬미 and 꼬미 sniffer project)</i>"]
+];
