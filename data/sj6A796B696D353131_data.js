b601ea71c916c4b52a2fbac2d1ca6f0f6c49905a
gtcGameNb <laon.makers@yahoo.com>
2025-11-30 18:23:11 -0500

    The 2nd day update: C language structure in depth. Variable and function names. Pre-processors and data types.

--- data/sj6A796B696D353131_data.js
+++ data/sj6A796B696D353131_data.js
@@ -1,4 +1,5 @@
 let refRt = ["1-38-30"];
 let dateTimeLog = [
+    [2,  "2025.11.30", "14:00", "17:30", "3:30",  -1,    0,      0.667,   3, "The 2nd Day.<br><i style='color:grey;'>Variable and Function (names and structure), pre-processor, data types</i>"],
     [1,  "2025.11.23", "14:00", "17:30", "3:30",  -1,    20,     3.667,   3, "First Day.<br><i style='color:grey;'>C Basics. High level project design(꼬미 and 꼬미 sniffer project)</i>"]
 ];
