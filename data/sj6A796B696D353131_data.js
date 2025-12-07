6622881452ff1e6ea3a4faa978edc18ac7aa0f2a
gtcGameNb <laon.makers@yahoo.com>
2025-12-07 18:16:57 -0500

    The 3rd day update: details of data types. VIR, Vcc, GND via the story of water powred fan invention. Blink prj and modification.

--- data/sj6A796B696D353131_data.js
+++ data/sj6A796B696D353131_data.js
@@ -1,5 +1,6 @@
 let refRt = ["1-38-30"];
 let dateTimeLog = [
-    [2,  "2025.11.30", "14:00", "17:30", "3:30",  -1,    0,      0.667,   3, "The 2nd Day.<br><i style='color:grey;'>Variable and Function (names and structure), pre-processor, data types</i>"],
-    [1,  "2025.11.23", "14:00", "17:30", "3:30",  -1,    20,     3.667,   3, "First Day.<br><i style='color:grey;'>C Basics. High level project design(꼬미 and 꼬미 sniffer project)</i>"]
+    [3,  "2025.12.07", "14:00", "17:40", "3:40",  -1,    20,     4.334,   3, "The 3rd Day: <i style='color:grey;'>Data type details(variable name == '문패').<br> VIR, Vcc, and GND via '궁녀와장인'. Blink example prj.</i>"],
+    [2,  "2025.11.30", "14:00", "17:30", "3:30",  -1,    0,      0.667,   3, "The 2nd Day: <i style='color:grey;'>Variable and Function (names and structure),<br> pre-processor, data types</i>"],
+    [1,  "2025.11.23", "14:00", "17:30", "3:30",  -1,    20,     3.667,   3, "First Day: <i style='color:grey;'>C Basics. High level project design(꼬미 and <br>꼬미 sniffer project)</i>"]
 ];
