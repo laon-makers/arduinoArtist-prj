60785b47f064f378f7980ebfca59ef2aff3d5889
gtcGameNb <laon.makers@yahoo.com>
2025-12-14 19:36:56 -0500

    The 4th day update: bit operations, operation orders, variable scopes, value assginments (overflow and loss of decimal/fractional part; loss of value flavor).

--- data/sj6A796B696D353131_data.js
+++ data/sj6A796B696D353131_data.js
@@ -1,5 +1,6 @@
 let refRt = ["1-38-30"];
 let dateTimeLog = [
+    [4,  "2025.12.14", "15:00", "18:30", "3:30",  -1,    0,      1.334,   3, "The 4th Day: <i style='color:grey;'>Bit operations, operation orders, variable scopes, <br>value assignments: overflow &amp; loss of flavor (loss of decimal/fractional part).</i>"],
     [3,  "2025.12.07", "14:00", "17:40", "3:40",  -1,    20,     4.334,   3, "The 3rd Day: <i style='color:grey;'>Data type details(variable name == '문패').<br> VIR, Vcc, and GND via '궁녀와장인'. Blink example prj.</i>"],
     [2,  "2025.11.30", "14:00", "17:30", "3:30",  -1,    0,      0.667,   3, "The 2nd Day: <i style='color:grey;'>Variable and Function (names and structure),<br> pre-processor, data types</i>"],
     [1,  "2025.11.23", "14:00", "17:30", "3:30",  -1,    20,     3.667,   3, "First Day: <i style='color:grey;'>C Basics. High level project design(꼬미 and <br>꼬미 sniffer project)</i>"]
