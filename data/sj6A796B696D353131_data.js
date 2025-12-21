4377a0c59416b4640f571a693ae7603bc9abad4c
gtcGameNb <laon.makers@yahoo.com>
2025-12-21 18:25:20 -0500

    The 5th day update: blink example and its application. How to analize the blink in terms of the voltage and current. The 'if' statement and its usage.

--- data/sj6A796B696D353131_data.js
+++ data/sj6A796B696D353131_data.js
@@ -1,5 +1,6 @@
 let refRt = ["1-38-30"];
 let dateTimeLog = [
+    [5,  "2025.12.21", "14:00", "18:00", "4:00",  -1,    20,     5,       3, "The 5th Day: <i>Blink example and its application.</i> <br><i style='color:grey;'>How it is blinked with voltage (the height of waterfall) and<br> the current (the flow of water). The 'if' statement and its application.</i>"],
     [4,  "2025.12.14", "15:00", "18:30", "3:30",  -1,    0,      1.334,   3, "The 4th Day: <i style='color:grey;'>Bit operations, operation orders, variable scopes, <br>value assignments: overflow &amp; loss of flavor (loss of decimal/fractional part).</i>"],
     [3,  "2025.12.07", "14:00", "17:40", "3:40",  -1,    20,     4.334,   3, "The 3rd Day: <i style='color:grey;'>Data type details(variable name == '문패').<br> VIR, Vcc, and GND via '궁녀와장인'. Blink example prj.</i>"],
     [2,  "2025.11.30", "14:00", "17:30", "3:30",  -1,    0,      0.667,   3, "The 2nd Day: <i style='color:grey;'>Variable and Function (names and structure),<br> pre-processor, data types</i>"],
