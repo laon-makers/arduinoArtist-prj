8e9af803afc437efb4067ab02b80d39b07896d07
gtcGameNb <laon.makers@yahoo.com>
2026-02-01 18:02:50 -0500

    The 7th day update: add the yellow light to the traffic control project. Install bit and learn how to use it.

--- data/sj6A796B696D353131_data.js
+++ data/sj6A796B696D353131_data.js
@@ -1,9 +1,10 @@
 let refRt = ["1-38-30"];
 let dateTimeLog = [
+    [7,  "2026.2.1",   "14:00", "17:40", "3:40",  -1,    20,     5.667,   3, "The 7th Day: <i>Added the yellow light to the traffic lights. Installed git and committed changes.</i>"],
     [6,  "2025.12.28", "14:15", "18:00", "3:45",  -1,     0,     2,       3, "The 6th Day: <i>Controlling the traffic lights.</i><br><i style='color:grey;'>Repeat the process of turning the green light on for 2 seconds,<br>then the red light on for 2 seconds.</i>"],
     [5,  "2025.12.21", "14:00", "18:00", "4:00",  -1,    20,     5,       3, "The 5th Day: <i>Blink example and its application.</i><br><i style='color:grey;'>How it is blinked with voltage (the height of waterfall) and<br> the current (the flow of water). The 'if' statement and its application.</i>"],
-    [4,  "2025.12.14", "15:00", "18:30", "3:30",  -1,    0,      1.334,   3, "The 4th Day: <i style='color:grey;'>Bit operations, operation orders, variable scopes, <br>value assignments: overflow &amp; loss of flavor (loss of decimal/fractional part).</i>"],
+    [4,  "2025.12.14", "15:00", "18:30", "3:30",  -1,     0,     1.334,   3, "The 4th Day: <i style='color:grey;'>Bit operations, operation orders, variable scopes, <br>value assignments: overflow &amp; loss of flavor (loss of decimal/fractional part).</i>"],
     [3,  "2025.12.07", "14:00", "17:40", "3:40",  -1,    20,     4.334,   3, "The 3rd Day: <i style='color:grey;'>Data type details(variable name == '문패').<br> VIR, Vcc, and GND via '궁녀와장인'. Blink example prj.</i>"],
-    [2,  "2025.11.30", "14:00", "17:30", "3:30",  -1,    0,      0.667,   3, "The 2nd Day: <i style='color:grey;'>Variable and Function (names and structure),<br> pre-processor, data types</i>"],
+    [2,  "2025.11.30", "14:00", "17:30", "3:30",  -1,     0,     0.667,   3, "The 2nd Day: <i style='color:grey;'>Variable and Function (names and structure),<br> pre-processor, data types</i>"],
     [1,  "2025.11.23", "14:00", "17:30", "3:30",  -1,    20,     3.667,   3, "First Day: <i style='color:grey;'>C Basics. High level project design(꼬미 and <br>꼬미 sniffer project)</i>"]
 ];
