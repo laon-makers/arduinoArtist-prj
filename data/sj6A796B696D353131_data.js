4402b4b10412b50cb96c411b2aaca066a8fdf274
gtcGameNb <laon.makers@yahoo.com>
2026-04-26 18:16:49 -0400

    The 14th day: made a custom timer and created new branch for a blocking delay.

--- data/sj6A796B696D353131_data.js
+++ data/sj6A796B696D353131_data.js
@@ -1,5 +1,6 @@
 let refRt = ["1-38-30"];
 let dateTimeLog = [
+    [14, "2026. 4.26", "14:00", "18:00", "4:00",  -1,     0,     4.667,   3, "<i>Made a custom timer for none blocking delay."],
     [13, "2026. 4.12", "14:00", "18:50", "4:50",  -1,    20,     7.667,   3, "<i>Fix: the unstoppable light on/off sequence issue.<br>Discuss about git branches and a blocking/non-blocking time delay.</i>"],
     [12, "2026. 4.05", "14:00", "17:50", "3:50",  -1,     0,     4,       3, "<i>Reminded/defined the class objectives. Reviewed how to convert lines of code into a func.<br>Moved delay func calls into the custom func.</i>"],
     [11, "2026. 3.08", "14:00", "18:30", "4:30",  -1,    20,     7,       3, "<i>Move a few function into newly created files; one source and one header file.<br>Use the VS code as an IDE.</i>"],
